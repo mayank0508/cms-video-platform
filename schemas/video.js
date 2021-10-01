c675ce614cc4c2f7215723779a45d8ba1f5c3ab0
mayank0508 <thakurmayank0508@gmail.com>
2021-10-02 00:19:04 +0530

    schemas

---
+++ schemas/video.js
@@ -0,0 +1,63 @@
+export default {
+  name: 'video',
+  title: 'Video',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validaton: Rule => Rule.required().warning('A Title is required')
+    },
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'number',
+      validaton: Rule => Rule.required().warning('An ID is required')
+    },
+    {
+      name: 'asset',
+      title: 'Asset',
+      type: 'mux.video'
+    },
+    {
+      name: 'thumbnail',
+      title: 'Thumbnail',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string'
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'date'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 100
+      },
+      validaton: Rule => Rule.required().warning('A Slug is required')
+    },
+    // {
+    //   name: 'author',
+    //   title: 'Author',
+    //   type: 'author',
+    // },
+    // {
+    //   name: 'interaction',
+    //   title: 'Interaction',
+    //   type: 'array',
+    //   of: [{ type: 'interaction' }]
+    // }
+  ]
+};
