c6125aaf3edf05afc23701e419dba251bb6d24f8
mayank0508 <thakurmayank0508@gmail.com>
2021-10-05 01:23:14 +0530

    account

--- schemas/account.js
+++ schemas/account.js
@@ -1,10 +1,48 @@
 export default {
-    name: 'account',
-    title: 'Account',
-    type: 'document',
-    fields: [
-        {
-
-        }
-    ]
-}
+  name: 'account',
+  title: 'Account',
+  type: 'document',
+  fields: [
+    {
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+      validaton: Rule => Rule.required().warning('Username is required')
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'username',
+        maxLength: 100
+      },
+      validaton: Rule => Rule.required().warning('Slug is required')
+    },
+    {
+      name: 'id',
+      title: 'ID',
+      type: 'number',
+      validaton: Rule => Rule.required().warning('ID is required')
+    },
+    {
+      name: 'avatar',
+      title: 'Avatar',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'followers',
+      title: 'Followers',
+      type: 'number'
+    }
+    // {
+    //   name: 'videos',
+    //   title: 'Videos',
+    //   type: 'array',
+    //   of: [{ type: 'video'}]
+    // }
+  ]
+};
